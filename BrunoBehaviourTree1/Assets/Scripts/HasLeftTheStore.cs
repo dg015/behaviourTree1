985f8c7c7f93695b67a1372e70d5c3472c25aed8
dg015 <bruno.barbosa8@yahoo.com>
2025-03-19 11:56:04 -0400

    Actually fixed the problem now

--- BrunoBehaviourTree1/Assets/Scripts/HasLeftTheStore.cs
+++ BrunoBehaviourTree1/Assets/Scripts/HasLeftTheStore.cs
@@ -1,29 +1,26 @@
 using NodeCanvas.Framework;
 using ParadoxNotion.Design;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace NodeCanvas.Tasks.Conditions {
 
-	public class SearchedCT : ConditionTask {
-		public BBParameter<int> NumberOfSearchedLocation;
-		public BBParameter <int> requiredNumberOfLocation;
-        public BBParameter<Transform> CurrentDestination;
-		public BBParameter<Transform> ExitLocation;
+	public class HasLeftTheStore : ConditionTask {
 
         //Use for initialization. This is called only once in the lifetime of the task.
         //Return null if init was successfull. Return an error string otherwise
+        public BBParameter<int> requiredNumberOfLocation;
+        public BBParameter<int> NumberOfSearchedLocation;
+		public bool isOver;
         protected override string OnInit(){
 			return null;
 		}
 
 		//Called whenever the condition gets enabled.
 		protected override void OnEnable() {
-			//check if the character has compared all of the milk prices
-			if(NumberOfSearchedLocation.value >= requiredNumberOfLocation.value)
-			{
-				CurrentDestination.value = ExitLocation.value;
-			}
-		}
+
+
+        }
 
 		//Called whenever the condition gets disabled.
 		protected override void OnDisable() {
@@ -33,7 +30,28 @@ namespace NodeCanvas.Tasks.Conditions {
 		//Called once per frame while the condition is active.
 		//Return whether the condition is success or failure.
 		protected override bool OnCheck() {
-			return true;
+            HasItEnded();
+            if (isOver)
+            {
+				return false;
+            }
+			else
+			{
+                return true;
+            }
+           
+		}
+
+		private void HasItEnded()
+		{
+			if (requiredNumberOfLocation.value == NumberOfSearchedLocation.value)
+			{
+				isOver = true;
+			}
+			else
+			{
+				isOver = false;
+			}
 		}
 	}
 }
